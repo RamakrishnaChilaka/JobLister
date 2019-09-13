414c997dcf8b8264ef93907f0d21a4bda34ce395
Chilaka Ramakrishna <ramakrishna@nference.net>
2019-09-13 16:30:35 +0530

    api index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,13 @@ import fetch from 'node-fetch';
 import data from './data.json';
 
 export function getData() {
-  return new Promise((res,rej) => {
+  /*return new Promise((res,rej) => {
     setTimeout(() => {
       res(data);
     }, 300);
-  });
+  });*/
 
-  // return fetch('https://jobsqared.herokuapp.com/jobs').then(data => data.json());
+  return fetch('https://jobsqared.herokuapp.com/jobs').then(data =>
+    data.json()
+  );
 }
-
